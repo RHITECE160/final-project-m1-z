89dd166252f4f955f982401faad697a6641a58f6
onstotjj <152225220+onstotjj@users.noreply.github.com>
2024-01-25 22:54:24 -0500

    For milestone 2
    Download both and put them in the same folder. 
    Functions is the setup from before and the functions. 
    Milestone 2 is the new setup and pin definitions. 
    LineFollowing() function is missing a definition and is commented out.
     this can be adapted from the line following code in examples.

---
+++ Main_Code/MileStone2.ino
@@ -0,0 +1,63 @@
+// Jocelyn, Ian, Owen
+// 1/23/24
+//The program causes the robot to move baced on the ps and ir controlers
+// The harware used was the PS and IR controlers and recivers and the robot
+// there are funtions for each of the movements and functions to relate butons to the movement funtions 
+// 
+// Load libraries used
+#include "SimpleRSLK.h"
+#include "Servo.h"
+#include "PS2X_lib.h"
+#include <TinyIRremote.h>
+#include <SharpDistSensor.h>
+// Helper macro for getting a macro definition as string
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+// Define pin numbers for the button on the PlayStation controller
+#define PS2_DAT 14  //P1.7 <-> brown wire
+#define PS2_CMD 15  //P1.6 <-> orange wire
+#define PS2_SEL 34   //P2.3 <-> yellow wire (also called attention)
+#define PS2_CLK 35   //P6.7 <-> blue wire
+// Define pin numbers for the button on the  IR controler
+#define sensorPin 26 //P4.4 <-> ???????
+#define IR_RCV_PIN 19  // P2.5 <-> black wire
+IRreceiver irRX(IR_RCV_PIN);
+
+// Create an instance of the playstation controller object
+PS2X ps2x;
+IRData IRresults;
+// Define remote mode either playstation controller or IR remote controller
+enum RemoteMode {
+  PLAYSTATION,
+  IR_REMOTE
+};
+
+// Declare and initialize the current state variable
+RemoteMode CurrentRemoteMode = IR_REMOTE;
+
+// Tuning Parameters
+const uint16_t lowSpeed = 15;
+const uint16_t fastSpeed = 30;
+
+// Window size of the median filter (odd number, 1 = no filtering)
+const byte medianFilterWindowSize = 5;
+
+// Create an object instance of the SharpDistSensor class
+SharpDistSensor sensor(sensorPin, medianFilterWindowSize);
+
+// Defining Distances 
+int stopDistance = 10;
+int error;
+int motorSpeed;
+
+Servo myservo;          // create servo object to control a servo
+int gripper_pos = 139;  // Gripper starts open
+void setup(){
+  
+}
+void loop(){
+  control();
+}
+
+
